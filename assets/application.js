4c0338aa36feccaa081664f2fad7d0234c89216b
Yun Chung <yun.chung@mac.com>
2020-12-04 18:13:45 +0900

    The add to cart button adds item(s) to cart with an ajax request

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,7 @@
 // Put your application javascript here
+console.log("testing from VS Code application.js file.....")
+
+// this function adds item(s) to cart without redirecting to the Cart page
+function addToCart(form) {
+    jQuery.post('/cart/add.js', $('form[action="/cart/add"]').serialize());
+}
