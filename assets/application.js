f731c4cef3a3b286ff842ff9af52d65010938b95
Yun Chung <yun.chung@mac.com>
2020-12-05 12:44:49 +0900

    A modal dialog appears when user accesses product page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,13 @@
 // Put your application javascript here
+
+// JS variables, event listeners, and function for closing product page modal
+const modal = document.querySelector("#product-page-modal");
+const modalHeaderButton = document.querySelector("#product-page-modal-header-button");
+const modalCloseButton = document.querySelector("#product-page-modal-close-button");
+
+function closeProductPageModal() {
+    modal.style.display = "none";
+}
+
+modalHeaderButton.addEventListener('click', closeProductPageModal);
+modalCloseButton.addEventListener('click', closeProductPageModal);
